652cfb6589ed0c6962d96b1f0864b562c337a78a
Chester Lynn <chester.htooaunglinn@gmail.com>
2021-02-24 15:33:12 +0630

    remove unused vars

--- src/components/Search/Error/Error.js
+++ src/components/Search/Error/Error.js
@@ -1,7 +1,7 @@
 import { Typography } from "@material-ui/core";
 import React from "react";
 
-const Error = (notFound) => {
+const Error = () => {
   return (
     <Typography
       variant="h5"
--- src/utils/checkValidUser.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const checkValidUser = (username) => {
-  axios.get(`https://api.github.com/users/${username}`).then((response) => {
-    console.log(response);
-  });
-};
